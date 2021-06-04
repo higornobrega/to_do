abcb2540f845df70242b64248322c706fff23768
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-04 14:18:22 -0300

    fixing bug

--- to_do/tarefa/models.py
+++ to_do/tarefa/models.py
@@ -4,7 +4,7 @@ from django.db.models.fields import CharField
 # Create your models here.
 class TarefaBd(models.Model):
     contents = models.CharField(max_length=1000)
-    date = models.DateField(auto_now_add=True)    
+    date = models.DateTimeField(auto_now_add=True)    
 
     def __str__(self):
         return str(self.id)
