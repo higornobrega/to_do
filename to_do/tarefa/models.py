6774dd19770db3e0ad75f38770e8e5b93921c210
Higor Nóbrega <higorst.nobrega@gmail.com>
2021-06-04 13:49:21 -0300

    Create the database in the Tarefa model

--- to_do/tarefa/models.py
+++ to_do/tarefa/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+class TarefaBd(models.Model):
+    contents = models.CharField(max_length=1000)
+    date = models.DateField(auto_now_add=True)    
+
+    def __str__(self):
+        return str(self.id)
